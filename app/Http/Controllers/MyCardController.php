375dd82dccdcad931e316f298dec67fbd389b503
nadiarostami <nadiarostami1894@gmail.com>
2025-04-25 19:18:35 +0330

    my card

--- app/Http/Controllers/MyCardController.php
+++ app/Http/Controllers/MyCardController.php
@@ -6,6 +6,7 @@ use App\Models\MyCard;
 use App\Models\MyCardLink;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -47,8 +48,6 @@ class MyCardController extends Controller
 
     public function store(Request $request)
     {
-        Log::info('Storing card data', ['request_data' => $request->all()]);
-
         try {
             $data = $request->validate([
                 'title' => 'required|string',
@@ -64,46 +63,33 @@ class MyCardController extends Controller
                 'text_color' => 'nullable|string',
                 'avatar' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
                 'status' => 'in:active,inactive',
-                'user_id' => 'required|exists:users,id',
             ]);
 
-            Log::info('Validation passed', ['validated_data' => $data]);
+
+            $data['user_id'] = Auth::id();
 
             if ($request->hasFile('avatar')) {
-                Log::info('Avatar file received', ['file_name' => $request->file('avatar')->getClientOriginalName()]);
                 $file = $request->file('avatar');
                 $fileName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
                 $file->move(public_path('avatars'), $fileName);
                 $data['avatar'] = 'avatars/' . $fileName;
-            } else {
-                Log::info('No avatar file received');
             }
 
             $data['uuid'] = Str::uuid();
 
-            Log::info('Creating new card', ['data_to_create' => $data]);
-
             $card = MyCard::create($data);
-
-            Log::info('Card created successfully', ['created_card' => $card]);
 
             return response()->json([
                 'status' => 'success',
                 'data' => $card,
             ], 201);
         } catch (\Exception $e) {
-            Log::error('Error occurred while storing card', [
-                'error_message' => $e->getMessage(),
-                'stack_trace' => $e->getTraceAsString(),
-            ]);
-
             return response()->json([
                 'status' => 'error',
                 'message' => 'An error occurred while storing the card.',
             ], 500);
         }
     }
-
 
     public function update(Request $request, MyCard $myCard)
     {
